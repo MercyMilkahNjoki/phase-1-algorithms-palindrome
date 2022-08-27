bd53cf40a9367d5d03cd034ce084ea3b01017f49
MercyMilkahNjokiKahungu <mercy.njoki@student.moringaschool.com>
2022-08-27 11:49:03 +0300

    done assignmnet

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  newWord = word.split('')
+  reversedWord = newWord.reverse()
+  jointWord = reversedWord.join('')
+  if(word === jointWord){
+    return true
+  }else{
+    return false
+  }
 }
-
+//  isPalindrome('aba')
+//  isPalindrome('racecar')
+//  isPalindrome('a')
+//  isPalindrome('robot')
+//  isPalindrome('ab')
 /* 
-  Add your pseudocode here
+  PSEUDOCODE
+  1.use split method to split the word 
+  2.use reverse method to reverse the split word 
+  3.use the join method to join the reversed word
+  3.use if statement to check if the word is equal to joint word
+  4.return true if the palindrome test passes and false if it fails
 */
 
 /*
-  Add written explanation of your solution here
+EXPLANATION
+first we spli the word to return an array then we reverse the strings inside the array using the reverse method,
+giving use a new reversed array. We the join the string inside the reversed array using
+the join method, which returns a joint stringarray. we then compare the word passed with
+the joint word and if the palindrome test results to true the word is the palindrom and test 
+otherwise if the result is false
 */
 
 // You can run `node index.js` to view these console logs
